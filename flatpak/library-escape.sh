d95b188bea2b2d26b2a779fb9b8c5244e809e28d
Ryan Brue <ryanbrue@hotmail.com>
2024-05-15 20:24:00 -0500

    Update library-escape.sh

--- flatpak/library-escape.sh
+++ flatpak/library-escape.sh
@@ -1,3 +1,4 @@
 export LIBRARY_PATH=/run/host/$(flatpak-spawn --host sh -c "echo $LIBRARY_PATH")
 export FLATPAK_USER_DIR=$HOME/.local/share/flatpak
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$LIBRARY_PATH
 $@
