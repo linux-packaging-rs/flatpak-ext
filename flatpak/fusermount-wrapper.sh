7bd7c6cb17abc2735c37ae9f7ecf4eaf10a04593
Ryan Brue <ryanbrue@hotmail.com>
2024-05-23 18:57:32 -0500

    wip: Flatpak manifest changes

--- flatpak/fusermount-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$_FUSE_COMMFD" ]; then
-    FD_ARGS=
-else
-    FD_ARGS="--env=_FUSE_COMMFD=${_FUSE_COMMFD} --forward-fd=${_FUSE_COMMFD}"
-fi
-
-exec flatpak-spawn --host $FD_ARGS fusermount "$@"
